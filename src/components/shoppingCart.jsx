45b399221ad7f7f2ba08d43a964720bc242056bf
liber09 <linda.bergsangel@hotmail.com>
2023-10-13 04:56:26 +0200

    Cart can now show productcount

--- src/components/shoppingCart.jsx
+++ src/components/shoppingCart.jsx
@@ -1,32 +1,26 @@
-//import { useNavigate } from "react-router-dom"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import shoppingCart from "../assets/shopping-cart.svg"
 import "./shoppingCart.css"
 
 export default function ShoppingCart({ productsCount }){
-
-  useEffect => {
-    productsCount = 5;
-  }
-  //const navigate = useNavigate()
-
-  //const navigateToCart = () => {
-  //  navigate("/cart")
-  //}
-  return(
-    <div>
-        <button className="container">
-            <span className="productsCount"></span>
-            <img src={shoppingCart} className="shoppingCart" alt="Go to Cart" />
-        </button>
-    </div>
-  )
-/*
-  return (
-    <button className={classes.container} onClick={navigateToCart}>
-      <span className={classes.productsCount}>{productsCount}</span>
-      <img src={shoppingCart} className={classes.shoppingCart} alt="Go to Cart" />
-    </button>
+    const [className, setClassName] = useState("productsCountZero")
     
-  )*/
+
+    useEffect(() => {
+        if (productsCount===undefined){
+            setClassName("productsCountZero")
+        }else{
+            setClassName("productsCount")
+        }
+    });
+    return(
+        <div>
+            <button className="container">
+                <span className={className}>
+                    {productsCount}
+                </span>
+                <img src={shoppingCart} className="shoppingCart" alt="Go to Cart" />
+            </button>
+        </div>
+    )
 }
